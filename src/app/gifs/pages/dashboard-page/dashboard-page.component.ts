ce91c6dee11c84cb5e343e5ebe434d040328f1b2
daderas <daderas@innovacion.gob.sv>
2025-03-25 14:39:13 -0600

    refactor en orden de carpetas

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import SideMenuHeaderComponent from "../../components/side-menu-header/side-menu-header.component";
-import SideMenuOptionsComponent from "../../components/side-menu-options/side-menu-options.component";
+import SideMenuHeaderComponent from "../../components/side-menu/side-menu-header/side-menu-header.component";
+import SideMenuOptionsComponent from "../../components/side-menu/side-menu-options/side-menu-options.component";
 import SideMenuComponent from "../../components/side-menu/side-menu.component";
 
 @Component({
